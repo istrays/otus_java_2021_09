6996a1ddf74dc29b35f0397cc666cded72ba99e1
straysizmailova@gmail.com <1esT9i6ag>
2021-10-12 21:31:04 +0300

    hw01-gradle: create fat jar with guava call

---
+++ hw01-gradle/src/main/java/ru/strays/HelloOtus.java
@@ -0,0 +1,11 @@
+package ru.strays;
+
+import com.google.common.collect.Lists;
+
+import java.util.Arrays;
+
+public class HelloOtus {
+    public static void main(String[] args) {
+        System.out.println(Lists.reverse(Arrays.asList(1, 2, 3, 4)));
+    }
+}
