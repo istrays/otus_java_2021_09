71356a2549af16b96059ebbe2b55b7ee6441dcf8
straysizmailova@gmail.com <1esT9i6ag>
2021-10-21 13:43:40 +0300

    hw04-generics: Fix method's order

--- hw04-generics/src/main/java/homework/CustomerService.java
+++ hw04-generics/src/main/java/homework/CustomerService.java
@@ -7,13 +7,6 @@ public class CustomerService {
 
     private final NavigableMap<Customer, String> storage = new TreeMap<>(Comparator.comparingLong(Customer::getScores));
 
-    private Map.Entry<Customer, String> copyEntry(Map.Entry<Customer, String> original) {
-        return Optional
-                .ofNullable(original)
-                .map(entry -> new AbstractMap.SimpleEntry<>(new Customer(entry.getKey()), entry.getValue()))
-                .orElse(null);
-    }
-
     public Map.Entry<Customer, String> getSmallest() {
         Map.Entry<Customer, String> originalEntry = storage.firstEntry();
         return copyEntry(originalEntry);
@@ -26,5 +19,12 @@ public class CustomerService {
 
     public void add(Customer customer, String data) {
         storage.put(customer, data);
+    }
+
+    private Map.Entry<Customer, String> copyEntry(Map.Entry<Customer, String> original) {
+        return Optional
+                .ofNullable(original)
+                .map(entry -> new AbstractMap.SimpleEntry<>(new Customer(entry.getKey()), entry.getValue()))
+                .orElse(null);
     }
 }
